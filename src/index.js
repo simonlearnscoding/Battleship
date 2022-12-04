3f1d96370d6508e09f4bd160c6a343cecad591f1
Simone Muscas <simon.muscas@gmail.comn>
2022-12-04 19:04:38 +0100

    fist commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import components from your components
+import './styles/style.css';
+
+// do something on site load
+//  document.addEventListener('DOMContentLoaded', UI.loadHomepage) //typo for testing
+
+
+
+
+// use icons like this:
+
+// import Icon from './icon.png';
+// Add the image to our existing div.
+
+// const myIcon = new Image();
+
+// myIcon.src = Icon;
+
+
+// element.appendChild(myIcon);
